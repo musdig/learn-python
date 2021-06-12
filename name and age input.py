1510af40218e67ebea5fa14b4a8204b0bb4ce27c
musdig <79258598+musdig@users.noreply.github.com>
2021-06-12 07:08:37 -0400

    Add files via upload

---
+++ name and age input.py
@@ -0,0 +1,6 @@
+name = input("What is your name?")
+age = int(input("How old are you?"))
+time = 100 - age
+c = int(input("How many times do you want this printed?"))
+
+print(c * ("Hello " + name + " you will be 100yrs old in " + str(time) +"yrs \n"))
